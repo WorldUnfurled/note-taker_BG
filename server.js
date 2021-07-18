eb7d5c73868c80ee349a87c394d2d5f03887f810
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-17 23:43:52 -0400

    Added routes to server

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ const PORT = process.env.PORT || 8080;
 // Express encoding
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+// console.log(__dirname) = ...\note-taker_BG
