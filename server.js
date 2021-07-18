4748d4e7191566dbe153bac8ee32d689e88bf00a
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-17 23:46:46 -0400

    Added listener to port 8080

--- server.js
+++ server.js
@@ -16,4 +16,6 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
 // console.log(__dirname) = ...\note-taker_BG
