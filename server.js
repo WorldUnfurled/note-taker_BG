b33a82aaf3291b8c471a5f17bc9a3728602c1e7b
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-17 23:38:25 -0400

    Added express call, port specification, and encoding specifications

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
+// Imports
 const express = require('express');
 const path = require('path');
+
+// Express call
+const app = express();
+
+// Port specification
+const PORT = process.env.PORT || 8080;
+
+// Express encoding
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
